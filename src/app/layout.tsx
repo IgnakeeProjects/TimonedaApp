d7fd3f90d975de7ac35544b3fab4887e64db281f
arlexrush <arlexrush@gmail.com>
2025-09-26 20:54:01 +0200

    feature: Create Layout vers 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
               <Link href="/registro">Registro</Link>
             </div>
             {/* Icono de menú para móviles */}
-            <button className="md:hidden">
+            <button className="md:hidden" title="Abrir menú" aria-label="Abrir menú">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
               </svg>
