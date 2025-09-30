e3bf666d7204ddc03315b6ec13328a96ebb4096e
arlexrush <arlexrush@gmail.com>
2025-09-30 10:31:30 +0200

    Fix, Create header, footer and Post Section

--- next.config.ts
+++ next.config.ts
@@ -1,36 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  //output: 'export',
-  images: {
-    unoptimized: true
-  },
-  // Optimizaciones para reducir el tamaño del bundle
-  compress: true,
-  trailingSlash: true,
-  // Configuración para Azure Static Web Apps
-  experimental: {
-    // Optimizar el warm-up
-    optimizePackageImports: ['lucide-react', '@radix-ui/react-icons'],
-  },
-   async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on'
-          },
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains'
-          },
-        ],
-      },
-    ];
-  },
+  // No necesitas 'output', 'images.unoptimized' ni nada especial
+  // Vercel lo maneja todo automáticamente
 };
 
-module.exports = nextConfig;
-
+export default nextConfig;
